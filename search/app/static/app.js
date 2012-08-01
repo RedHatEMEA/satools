a2ce373c62106f2487960af096596385d0658204
Jim Minter <jminter@redhat.com>
2012-08-01 11:50:57 +0100

    fix search layout for extjs-4.1

--- search/app/static/app.js
+++ search/app/static/app.js
@@ -17,36 +17,47 @@ Ext.application({
     launch: function() {
 	Ext.create("Ext.container.Viewport", {
 	    layout: {
-		type: "vbox",
-		align: "center"
+		type: "hbox",
+		align: "stretch"
 	    },
 	    items: [{
 		border: 0,
 		flex: 1
 	    }, {
-		xtype: "search",
-		width: "80%"
-	    }, {
-		border: 0,
-		flex: 1
-	    }, {
-		xtype: "results",
-		itemId: "myresults",
-		width: "80%",
-		flex: 12
-	    }, {
-		border: 0,
-		flex: 1
-	    }, {
-		xtype: "panel",
-		layout: "fit",
+		layout: {
+		    type: "vbox",
+		    align: "stretch"
+		},
 		items: [{
-		    xtype: "message",
-		    itemId: "mypanel"
+		    border: 0,
+		    flex: 1
+		}, {
+		    xtype: "search"
+		}, {
+		    border: 0,
+		    flex: 1
+		}, {
+		    xtype: "results",
+		    itemId: "myresults",
+		    flex: 12
+		}, {
+		    border: 0,
+		    flex: 1
+		}, {
+		    xtype: "panel",
+		    layout: "fit",
+		    items: [{
+			xtype: "message",
+			itemId: "mypanel"
+		    }],
+		    flex: 20
+		}, {
+		    border: 0,
+		    flex: 1
 		}],
-		width: "80%",
-		flex: 20
-	    }, {
+		border: 0,
+		flex: 8
+	    },{
 		border: 0,
 		flex: 1
 	    }]
