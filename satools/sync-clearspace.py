f67a6c99a59ff8987c9a8ad2e55e49fbb0542efd
Jim Minter <jminter@redhat.com>
2013-05-16 17:54:25 +0100

    new sync-clearspace

--- satools/sync-clearspace.py
+++ satools/sync-clearspace.py
@@ -1,168 +1,224 @@
-#!/usr/bin/python
+#!/usr/bin/python -u
 
-import argparse
 import common
 import itertools
 import lxml.etree
 import os
 import Queue
 import re
+import requests
+import sys
 import threading
 import time
 import traceback
-import urllib2
+import urllib
 
-q = Queue.Queue()
-threadlock = threading.Lock()
+
+retries = 10
+step = 50
+q = Queue.Queue(step)
+lock = threading.Lock()
+files = set()
+tls = threading.local()
+
+
+class Download(object):
+  def __init__(self, href, path):
+    self.href = href
+    self.path = path
+
+  def download(self, mtime):
+    r = get(self.href, stream = True)
+
+    p = os.path.split(self.path)
+    tempname = os.path.join(p[0], "." + p[1])
+
+    with open(tempname, "wb") as f:
+      for data in r.iter_content(4096):
+        f.write(data)
+
+      f.flush()
+      os.fsync(f.fileno())
+
+    os.utime(tempname, (mtime, mtime))
+    common.mkro(tempname)
+    common.rename(tempname, self.path)
+
 
 class HTML(object):
-    def __init__(self, data):
-        parser = lxml.etree.HTMLParser()
-        self.xml = lxml.etree.fromstring(data.replace("\r\n", "\n"), parser)
-
-class WikiDoc(HTML):
-    def __init__(self, data):
-        super(WikiDoc, self).__init__(data)
-
-        self.filehref = self.xml.xpath("//span[@class='jive-wiki-body-file-info']/a/@href")[0]
-
-        self.filename = self.xml.xpath("//span[@class='jive-wiki-body-file-info']/a/text()")[0]
-
-        xp = self.xml.xpath("//div[@class='jive-content-header-details']")[0]
-        text = lxml.etree.tostring(xp, method = "text", encoding = "UTF-8")
-        r = re.search("Last Modified:.*?\n(.*?)\nby", text, re.S)
-        self.mtime = time.mktime(time.strptime(r.group(1), "%b %d, %Y %I:%M %p"))
-        xp = self.xml.xpath("//div[@id='jive-breadcrumb']/span/a")
-        xp = map(lambda x: x.text.strip().replace("/", "_"), xp)
-        self.path = "/".join(xp[:-1])
-
-        xp = self.xml.xpath("//div[@class='jive-content-header-version']/text()")[0]
-        r = re.search("VERSION (\d+)", xp)
-        self.version = r.group(1)
-
-class DocIndex(HTML):
-    def __init__(self, data):
-        super(DocIndex, self).__init__(data)
-
-        xp = self.xml.xpath("//td[@class='jive-table-cell-subject']/a")
-        self.items = map(lambda x: { "href": x.get("href"),
-                                     "title": x.text.strip() }, xp)
-
-def parse_args():
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-a", action="store_true", dest = "all",
-                    help = "check all downloads for updates")
-
-    return vars(ap.parse_args())
-
-def download(item, db, tries):
-    if item["href"] in db:
-        path = db.get(item["href"])
-
-    else:
-        f = common.retrieve_m(config["clearspace-root"] + item["href"],
-                              tries = tries)
-        doc = WikiDoc(f.read())
-        f.close()
-
-        path = doc.path + "/" + doc.filename
-                
-        if want(path):
-            skip = False
-            if os.path.exists(path):
-                st = os.stat(path)
-                if st.st_mtime == doc.mtime:
-                    skip = True
-
-            if not skip:
-                common.mkdirs(doc.path)
-                common.retrieve(config["clearspace-root"] + doc.filehref,
-                                path, force = True, tries = tries)
-                common.mkro(path)
-                os.utime(path, (doc.mtime, doc.mtime))
-
-    updatedbs(db, keep, item["href"], path)
+  def __init__(self, data):
+    parser = lxml.etree.HTMLParser()
+    self.xml = lxml.etree.fromstring(data.replace("\r\n", "\n"), parser)
 
 
-def want(path):
-    return not (config["clearspace-odponly"] == "1" and not
-                path.lower().endswith(".odp"))
+class Document(HTML):
+  def __init__(self, data):
+    super(Document, self).__init__(data)
+
+    details = self.xml.xpath("//div[@class='jive-content-header-details']")[0]
+    text = lxml.etree.tostring(details, method = "text", encoding = "UTF-8")
+    r = re.search("Last Modified:.*?\n(.*?)\nby", text, re.S)
+    self.mtime = time.mktime(time.strptime(r.group(1), "%b %d, %Y %I:%M %p"))
+
+    insert = None
+    self.path = self.xml.xpath("//div[@id='jive-breadcrumb']/span/a")
+    if self.path[1].get("href").startswith("/groups/"):
+      insert = "Groups"
+    elif self.path[1].get("href").startswith("/community/"):
+      insert = "Communities"
+    self.path = [p.text.strip().replace("/", "_") for p in self.path][:-1]
+    if insert:
+      self.path.insert(1, insert)
+
+
+class AttachDocument(Document):
+  def __init__(self, data):
+    super(AttachDocument, self).__init__(data)
+
+    title = self.xml.xpath("//div[@class='jive-content-title']/h2")[0]
+    self.path += [title[0].tail.strip()]
+
+    hrefs = self.xml.xpath("//div[@class='jive-attachments']//a/@href")
+    self.downloads = []
+    for href in hrefs:
+      dlpath = self.path + [urllib.unquote(href.split("/")[-1]).decode("utf-8")]
+      self.downloads.append(Download(href, "/".join(dlpath)))
+
+
+class BinaryDocument(Document):
+  def __init__(self, data):
+    super(BinaryDocument, self).__init__(data)
+
+    href = self.xml.xpath("//span[@class='jive-wiki-body-file-info']/a/@href")[0]
+    dlpath = self.path + [urllib.unquote(href.split("/")[-1]).decode("utf-8")]
+
+    self.downloads = [Download(href, "/".join(dlpath))]
+
+
+class Index(HTML):
+  def __init__(self, data):
+    super(Index, self).__init__(data)
+
+    xp = self.xml.xpath("//td[@class='jive-table-cell-subject']/a")
+    self.hrefs = [x.get("href") for x in xp]
+
+
+def get(href, **kwargs):
+  for i in range(retries):
+    try:
+      log(href)
+      r = tls.s.get(config["clearspace-root"] + href,
+                    auth = (config["clearspace-username"],
+                            config["clearspace-password"]),
+                    headers = { "Accept-Language": "en" },
+                    **kwargs)
+
+      if r.status_code != 200:
+        raise Exception("Bad status code %u with url: %s" % (r.status_code, href))
+
+      break
+
+    except Exception as e:
+      if i == retries - 1:
+        raise
+      
+      log("%s, sleeping..." % e.message
+      time.sleep(10)
+
+  return r
+
+def download(cls, href):
+  r = get(href)
+  doc = cls(r.text)
+
+  for dl in doc.downloads:
+    if not dl.path.lower().endswith(".odp"):
+      continue
+
+    with lock:
+      if dl.path in files:
+        continue
+      files.add(dl.path)
+
+    if not os.path.exists(dl.path) or os.path.getmtime(dl.path) != doc.mtime:
+      common.mkdirs(os.path.dirname(dl.path))
+      dl.download(doc.mtime)
 
 def worker():
-    while True:
-        items = q.get()
-        try:
-            items[0](*items[1:])
-        except Exception, e:
-            traceback.print_exc()
-        q.task_done()
+  tls.s = requests.Session()
+  while True:
+    items = q.get()
+    try:
+      items[0](*items[1:])
+    except Exception:
+      log(str(items) + "\n" + traceback.format_exc())
+    q.task_done()
 
-def updatedbs(db, keep, href, path):
-    threadlock.acquire()
-    db.add(href, path)
-    if want(path):
-        keep.add(path)
-    threadlock.release()
+def iter_index(basehref, d, cls):
+  d["step"] = step
+  for i in itertools.count(step = step):
+    d["offset"] = i
+    r = get(basehref % d)
+    index = Index(r.text)
+
+    for href in index.hrefs:
+      q.put((download, cls, href))
+
+    if len(index.hrefs) != step:
+      break
+
+def users():
+  get("/")
+  for i in itertools.count(step = step):
+    r = get("/api/rest/userService/usersBounded/%u/%u" % (i, step))
+    xml = lxml.etree.fromstring(r.text)
+    ids = xml.xpath("//ID/text()")
+    for i in ids:
+      yield int(i)
+    if len(ids) != step:
+      break
+
+def cleanup():
+  for dirpath, dirnames, filenames in os.walk(".", topdown = False):
+    for f in filenames:
+      path = os.path.normpath(os.path.join(dirpath, f))
+      if path.decode("utf-8") not in files and path[0] != ".":
+        os.unlink(path)
+
+    if not os.listdir(dirpath):
+      os.rmdir(dirpath)
+
+def log(s):
+  with lock:
+    print >>sys.stderr, s
+
+def main():
+  global config
+  config = common.load_config()
+  config["clearspace-base"] = os.environ["HOME"] + "/docspace-2" # TODO: remove
+
+  common.mkdirs(config["clearspace-base"])
+  os.chdir(config["clearspace-base"])
+
+  l = common.Lock(".lock")
+
+  threads = int(config["clearspace-threads"])
+  for i in range(threads):
+    t = threading.Thread(target = worker, name = i)
+    t.daemon = True
+    t.start()
+
+  tls.s = requests.Session()
+
+  for i in users():
+    iter_index("/view-documents.jspa?start=%(offset)u&range=%(step)u&numResults=%(step)u&tags=&containerType=14&containerID=1&targetUser=%(user)u&filter=collab", {"user": i}, AttachDocument)
+    iter_index("/view-documents.jspa?start=%(offset)u&range=%(step)u&numResults=%(step)u&tags=&containerType=14&containerID=1&targetUser=%(user)u&filter=presentations", {"user": i}, BinaryDocument)
+
+  q.join()
+
+  cleanup()
+
+  common.write_sync_done()
 
 if __name__ == "__main__":
-    global config
-    config = common.load_config()
-    args = parse_args()
-
-    common.mkdirs(config["clearspace-base"])
-    os.chdir(config["clearspace-base"])
-
-    lock = common.Lock(".lock")
-
-    if args["all"]:
-        common.unlink(".sync-db")
-
-    db = common.DB(".sync-db")
-    db.cmpfn = lambda x, y: cmp(int(x.split("-")[1]), int(y.split("-")[1]))
-
-    pm = urllib2.HTTPPasswordMgrWithDefaultRealm()
-    pm.add_password(None, config["clearspace-root"],
-                    config["clearspace-username"],
-                    config["clearspace-password"])
-    opener = urllib2.build_opener(urllib2.HTTPBasicAuthHandler(pm))
-    urllib2.install_opener(opener)
-
-    threads = int(config["clearspace-threads"])
-    if threads > 1:
-        common.progress = lambda x, y: None
-        common.progress_finish = lambda: None
-
-    for i in range(threads):
-        t = threading.Thread(target = worker, name = i)
-        t.daemon = True
-        t.start()
-
-    keep = set()
-    step = 50
-    tries = 10
-    for i in itertools.count(step = step):
-        f = common.retrieve_m(config["clearspace-root"] + "/view-documents.jspa?start=%u&numResults=%u&filter=presentations" % (i, step), tries = tries)
-        index = DocIndex(f.read())
-        f.close()
-
-        for item in index.items:
-            q.put((download, item, db, tries))
-
-        if len(index.items) != step:
-            break
-
-    q.join()
-
-    for dirpath, dirnames, filenames in os.walk(".", topdown = False):
-        # remove local files which are no longer found in clearspace
-        for f in filenames:
-            path = os.path.relpath(dirpath, ".") + "/" + f
-            if not path.startswith("./.") and path not in keep:
-                os.unlink(path)
-
-        # prune empty local directories
-        if not os.listdir(dirpath):
-            os.rmdir(dirpath)
-
-    with open(".sync-done", "w") as f:
-        pass
+  main()
