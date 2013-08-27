4984f40779387de3bef6d167bed538fca458cd51
Jim Minter <jminter@redhat.com>
2013-08-27 12:46:12 +0100

    sync-pt improvements, python3

--- satools/sync-pt.py
+++ satools/sync-pt.py
@@ -1,102 +1,148 @@
 #!/usr/bin/python3 -u
 
 from satools import common
-import http.cookiejar
+import codecs
 import lxml.html
 import os
 import queue
+import requests
 import sys
 import threading
-import urllib.error
+import time
+import traceback
 import urllib.parse
-import urllib.request
 
+files = set()
+lock = threading.Lock()
 q = queue.Queue()
+tls = threading.local()
 
-def worker():
+def log(s):
+    with lock:
+        print(s, file = sys.stderr)
+
+def worker(cookies):
+    tls.s = requests.Session()
+    tls.s.cookies = cookies
+
     while True:
-        items = q.get()
-        items[0](*items[1:])
+        try:
+            items = q.get()
+            items[0](*items[1:])
+        except Exception:
+            log(str(items) + "\n" + traceback.format_exc())
         q.task_done()
 
 def want(path):
     return not (config["pt-odponly"] == "1" and not
                 path.lower().endswith(".odp"))
 
-def download(opener, href, dest):
-    common.mkdirs(os.path.split(dest)[0])
-    common.retrieve(href, dest, opener = opener, tries = 10)
-    common.mkro(dest)
-
-def read_events(opener, url):
-    try:
-        html = common.retrieve_m(url, opener = opener, tries = 10).read()
-    except urllib.error.HTTPError as e:
-        if e.code == 403:
-            print("WARNING: %s, continuing..." % e, file = sys.stderr)
+def download(url, path):
+    with lock:
+        if path in files:
             return
-        else:
-            raise
+        files.add(path)
 
-    html = lxml.html.fromstring(html.decode("utf-8"))
+    if os.path.exists(path):
+        r = tls.s.head(url)
+        mtime = common.parse_last_modified(r.headers["Last-Modified"])
+
+        if os.path.getmtime(path) == mtime and \
+           os.path.getsize(path) == int(r.headers["Content-Length"]):
+            return
+
+    common.mkdirs(os.path.dirname(path))
+
+    log(url + " -> " + path)
+    r = tls.s.get(url, stream = True)
+
+    temppath = common.mktemppath(path)
+
+    with open(temppath, "wb") as f:
+        for data in r.iter_content(4096):
+            f.write(data)
+
+        f.flush()
+        os.fsync(f.fileno())
+
+    mtime = common.parse_last_modified(r.headers["Last-Modified"])
+    os.utime(temppath, (mtime, mtime))
+    common.mkro(temppath)
+    common.rename(temppath, path)
+
+def read_events(url):
+    log(url)
+    r = tls.s.get(url)
+    if r.status_code != 200:
+        log("WARNING: %u on %s" % (r.status_code, url))
+        return
+
+    html = lxml.html.fromstring(r.text)
     
     path = html.xpath("//div[@id='breadcrumbs']//li[last()]//text()")[0].strip()
     for _url in html.xpath("//a[starts-with(@href, 'download/attachments/')]/@href"):
         dest = urllib.parse.unquote(_url)
         dest = path + "/" + "-".join(dest.rsplit("/", 2)[1:])
-        _url = urllib.parse.urljoin(url, _url)
 
         if want(dest):
-            q.put((download, opener, _url, dest))
+            q.put((download, urllib.parse.urljoin(url, _url), dest))
 
-def read_project_list(opener):
-    url = config["pt-root"] + "projects-emea/project-list"
-    html = common.retrieve_m(url, opener = opener, tries = 10).read()
-    html = lxml.html.fromstring(html)
+def read_project_list():
+    url = config["pt-root"] + "projects-emea/"
+    html = lxml.html.fromstring(tls.s.get(url).text)
 
-    for _url in html.xpath("//a[text()='events']/@href"):
-        _url = urllib.parse.urljoin(url, _url)
-        q.put((read_events, opener, _url))
+    for _url in html.xpath("//a[starts-with(@href, 'project-events?')]/@href"):
+        q.put((read_events, urllib.parse.urljoin(url, _url)))
 
-def login(opener):
+def login():
     url = config["pt-root"] + "register/"
-    html = common.retrieve_m(url, opener = opener, tries = 10).read()
-    html = lxml.html.fromstring(html)
+    html = lxml.html.fromstring(tls.s.get(url).text)
 
-    params = {}
-    for i in html.xpath("//form[@name = 'login']//input"):
-        params[i.get("name")] = i.get("value")
+    params = { i.get("name"): i.get("value") for i in html.xpath("//form[@name = 'login']//input") }
 
     params["email"] = config["pt-username"]
     params["password"] = config["pt-password"]
 
-    common.retrieve_m(url, urllib.parse.urlencode(params).encode("utf-8"),
-                      opener = opener, tries = 10)
+    tls.s.post(url, params)
 
-if __name__ == "__main__":
+def cleanup():
+    for dirpath, dirnames, filenames in os.walk(".", topdown = False):
+        for f in filenames:
+            path = os.path.normpath(os.path.join(dirpath, f))
+            if path not in files and path[0] != ".":
+                os.unlink(path)
+
+        if not os.listdir(dirpath):
+            os.rmdir(dirpath)
+
+def main():
     global config
     config = common.load_config()
+
+    # Permit write of UTF-8 characters to stderr (required when piping output)
+    if sys.stderr.encoding == None:
+        sys.stderr = codecs.getwriter("UTF-8")(sys.stderr.buffer)
 
     common.mkdirs(config["pt-base"])
     os.chdir(config["pt-base"])
 
-    lock = common.Lock(".lock")
+    _lock = common.Lock(".lock")
+    tls.s = requests.Session()
 
-    cj = http.cookiejar.CookieJar()
-    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+    login()
 
-    threads = int(config["pt-threads"])
-    if threads > 1:
-        common.progress = lambda x, y: None
-        common.progress_finish = lambda: None
-
-    for i in range(threads):
-        t = threading.Thread(target = worker, name = i)
+    for i in range(int(config["pt-threads"])):
+        t = threading.Thread(target = worker, name = i, args = [tls.s.cookies])
         t.daemon = True
         t.start()
 
-    login(opener)
-    read_project_list(opener)
+    read_project_list()
+    while q.unfinished_tasks:
+        time.sleep(1)
     q.join()
 
+    cleanup()
     common.write_sync_done()
+
+if __name__ == "__main__":
+    main()
